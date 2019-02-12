6d5e6b96412d2c784ca69bcb63d3ca0a0346639d
andrebeu <aobeukers@gmail.com>
2019-02-12 14:09:52 -0500

    fgate

--- fgate_analysis.py
+++ fgate_analysis.py
@@ -17,16 +17,17 @@ graphpr = 1
 
 seqL = ['AAA','ABA','BAA','BBA']
 
-nnets = 10
 ML = MetaLearner(lstm_size,nstories)
-for netn in range(nnets):
-  ML.reinitialize()
-  trainer = Trainer(ML,shift_pr=shiftpr,graph_pr=graphpr)
-  train_data = trainer.train_loop(nepochs)
-  # eval
-  for seq in seqL:
-    Xeval,Yeval = CSWMLTask(1).get_Xeval2(seq)
-    evalstep_data = trainer.eval_step(Xeval,Yeval)
-    fpath = 'eval_data/S%i-shift_%i-seq_%s'%(netn,100*shiftpr,seq)
-    np.save(fpath,evalstep_data)
+
+
+trainer = Trainer(ML,shift_pr=shiftpr,graph_pr=graphpr)
+train_data = trainer.train_loop(nepochs)
+
+netn = len(glob('eval_data/*'))
+# eval
+for seq in seqL:
+  Xeval,Yeval = CSWMLTask(1).get_Xeval2(seq)
+  evalstep_data = trainer.eval_step(Xeval,Yeval)
+  fpath = 'eval_data/S%i-shift_%i-seq_%s'%(netn,100*shiftpr,seq)
+  np.save(fpath,evalstep_data)
 
