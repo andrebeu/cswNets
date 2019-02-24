3fe42d6b6abac991e4c931d93a81cdf33e3abd21
andrebeu <aobeukers@gmail.com>
2019-02-24 11:58:19 -0500

    sweep fp6bp3

--- trainsave.py
+++ trainsave.py
@@ -10,14 +10,13 @@ import tensorflow as tf
 
 # model params
 stsize = int(sys.argv[1])
-nstories = 3
 # task/training params
-nepochs = 30000
+nepochs = 40000
 graphpr = int(sys.argv[2])
 shiftpr = int(sys.argv[3])
 
 
-ML = MetaLearner(stsize,nstories)
+ML = MetaLearner(stsize)
 
 # train
 trainer = Trainer(ML,shift_pr=shiftpr/100,graph_pr=graphpr/100)
@@ -27,13 +26,16 @@ train_data = trainer.train_loop(nepochs)
 eval_context_strL=["".join(i) for i in itertools.product(
                                     ('A1','A2','B1','B2'),
                                     ('A1','A2','B1','B2'),
+                                    ('A1','A2','B1','B2'),
+                                    ('A1','A2','B1','B2'),
+                                    ('A1','A2','B1','B2'),
                                     ('A1','A2','B1','B2')
                                    )]
 
 eval_data = trainer.eval_loop(eval_context_strL)
 
 ## save
-model_name = 'state_%i-csw_%i-nstories_%i-shiftpr_%i'%(stsize,nstories,shiftpr,graphpr)
+model_name = 'state_%i-csw_%i-shiftpr_%i-nstories_fp6bp3'%(stsize,graphpr,shiftpr)
 num_models = len(glob('models/csw_tpgen/%s/*'%model_name)) 
 model_dir = 'models/csw_tpgen/%s/%.3i'%(model_name,num_models) 
 os.makedirs(model_dir)
