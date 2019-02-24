968f096f59362aac255f1eebb2ba004558c5e7ce
andrebeu <aobeukers@gmail.com>
2019-02-24 11:29:57 -0500

    fp3bp3

--- rsync.sh
+++ rsync.sh
@@ -1,1 +1,1 @@
-rsync -r -vam abeukers@tigergpu.princeton.edu:/tigress/abeukers/wd/cswNets/models ./
+rsync -r -vam abeukers@tigergpu.princeton.edu:/tigress/abeukers/wd/cswNets/models/csw_tpgen/ ./models/csw_tpgen/
